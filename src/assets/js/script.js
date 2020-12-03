0e0b28b84287f806dbe5a0b04cb0b7a85d1c3613
Work <wrapped@grade1s-MacBook-Pro.local>
2020-12-03 22:26:16 +0000

    Updated sass

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,9 +1,11 @@
 (function ($) {
   "use strict"; // Start of use strict
 
-  $(document).ready(function () {
-   
-    console.log("script here")
-
-  });
+	$(document).ready(function () {
+		$('.product-slider-js').slick({
+			centerMode: true,
+			centerPadding: '60px',
+			slidesToShow: 3
+    	});
+  	});
 })(jQuery); // End of use strict
