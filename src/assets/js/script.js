39fa7c52ddbda3e2604a4b0f20dd4405cb40ab5d
Work <wrapped@grade1s-MacBook-Pro.local>
2020-12-04 18:46:38 +0000

    Updated structure and images

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -2,10 +2,16 @@
   "use strict"; // Start of use strict
 
 	$(document).ready(function () {
-		$('.product-slider-js').slick({
+
+		$('.slider__js').slick({
+			slidesToShow: 3,
+			slidesToScroll: 3,
 			centerMode: true,
 			centerPadding: '60px',
-			slidesToShow: 3
-    	});
-  	});
+			autoplay: true,
+			infinite: true,
+			autoplaySpeed: 2000,
+			arrows: false,
+		});
+	});
 })(jQuery); // End of use strict
