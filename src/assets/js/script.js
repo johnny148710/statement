01ac8cfa417ea34668144f8b5ccfb3be890448e4
Work <wrapped@grade1s-MacBook-Pro.local>
2020-12-06 18:54:54 +0000

    Added responsive styling

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -2,7 +2,7 @@
   "use strict"; // Start of use strict
 
 	$(document).ready(function () {
-
+		// Header
 		$(function() {
 			var header = $("#header-js");
 			$(window).scroll(function() {    
@@ -15,6 +15,30 @@
 				}
 			});
 		});
+
+		// Mobile button
+		$('.button__js').click(function(){
+			$('.nav-container').toggleClass('show');
+		});
+
+		(function() {
+
+			"use strict";
+			var toggles = document.querySelectorAll(".cmn-toggle-switch");
+			for (var i = toggles.length - 1; i >= 0; i--) {
+			  var toggle = toggles[i];
+			  toggleHandler(toggle);
+			};
+		  
+			function toggleHandler(toggle) {
+			  toggle.addEventListener( "click", function(e) {
+				e.preventDefault();
+				(this.classList.contains("active") === true) ? this.classList.remove("active") : this.classList.add("active");
+			  });
+			}
+		  
+		  })();
+
 		// Slider
 		$('.slider__js').slick({
 			slidesToShow: 3,
@@ -27,6 +51,43 @@
 			arrows: true,
 			prevArrow: $('.prev'),
 			nextArrow: $('.next'),
+			responsive: [
+				{
+				  breakpoint: 960,
+				  settings: {
+					centerMode: false,
+					slidesToShow: 2,
+					slidesToScroll: 2
+				  }
+				},
+				{
+				  breakpoint: 480,
+				  settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
+				  }
+				}
+			  ]
 		});
+
+		// Modal
+		var modal = document.getElementById("modal");
+		var btn = document.getElementById("modal-button");
+		var span = document.getElementsByClassName("close")[0];
+
+		btn.onclick = function(e) {
+			e.preventDefault;
+			modal.style.display = "block";
+			
+		}
+
+		span.onclick = function() {
+			modal.style.display = "none";
+		}
+		window.onclick = function(event) {
+			if (event.target == modal) {
+				modal.style.display = "none";
+			}
+		}
 	});
 })(jQuery); // End of use strict
